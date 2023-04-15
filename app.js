4927677d4812404e92f81d3fc93c5e3285b3ed13
Nasantogtohh <nasantogtokhbayanmunkh@gmail.com>
2023-04-15 22:46:39 +0800

    Done

--- app.js
+++ app.js
@@ -3,26 +3,165 @@
 //Dotor n huwisagch zarlwal window obectod  zarlgdna
 //closure gedeg n gadnah functyhaa huwisagchid handah
 //Degetstei ajillah controller
-var uiController = (function () {})();
-//Sanhuutee ajillah controller
-var financeController = (function () {})();
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function () {
+  //Daraan css zaswar hiiwel hergtei. HTML CSS tei holbootoi oorcloltond ortoj magadgu.
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+    incomeList: ".income__list",
+    expenseList: ".expenses__list",
+  };
+  // public service
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // exp, inc
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
 
-//Ehni 2 moduluudyg holboj oghoor,mon busad nemelt uildel
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+    clearFields: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputDescription + "," + DOMstrings.inputValue
+      );
+      //convert list to array
+      var fieldsArr = Array.prototype.slice.call(fields);
+      //element bolgonih n huwid hiih functin anonymous functeer damjuul ogy
+      fieldsArr.forEach(function (el, index, array) {
+        el.value = "";
+      });
+      //courseryg bairluulah 0 r elemt buyu description deer
+      fieldsArr[0].focus();
+      // for (var i = 0; i < fieldsArr.length; i++) {
+      //   fieldsArr[i].value = "";
+      // }
+    },
+    addListItem: function (item, type) {
+      // Орлого зарлагын элементийг агуулсан html-ийг бэлтгэнэ.
+      var html, list;
+      if (type === "inc") {
+        list = DOMstrings.incomeList;
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+      } else {
+        list = DOMstrings.expenseList;
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+      // Тэр HTML дотроо орлого зарлагын утгуудыг REPLACE ашиглаж өөрчилж
+      html = html.replace("%id%", item.id);
+      html = html.replace("$$DESCRIPTION$$", item.description);
+      html = html.replace("$$VALUE$$", item.value);
+
+      // Бэлтгэсэн HTML ээ DOM руу хийж өгнө.
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    },
+  };
+})();
+
+// Санхүүтэй ажиллах контроллер //Sanhuutee ajillah controller
+var financeController = (function () {
+  //baiguulagch funcgt ashiglan orlog zarlag hadgalah obect uusgeh
+  // private data
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // private data
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // private data
+  var data = {
+    items: {
+      inc: [],
+      exp: [],
+    },
+
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+      return item;
+    },
+
+    seeData: function () {
+      return data;
+    },
+  };
+})();
+
+// Програмын холбогч контроллер //Ehni 2 moduluudyg holboj oghoor,mon busad nemelt uildel
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
-    //1.Oruulah ogogdlyg beldetsees olj awna
-    //2.Olj awsan ogogdluudee sanhuugin controllert damjuulh tend hadgalna.
-    //3.Olj awsan ogogdluudee web deer tohiroh hesegt grgana.
-    //4.Tosowiig tootsoolno.
-    //5.Etssiin uldegdel,tootsoog delgetsend gargana.
+    // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+    var input = uiController.getInput();
+
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    var item = financeController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
+
+    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+    uiController.addListItem(item, input.type);
+    uiController.clearFields();
+    // 4. Төсвийг тооцоолно
+    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
   };
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
-  });
-  document.addEventListener("keypress", function (event) {
-    //Deer uyin browser deer ajillahin tuld event.which==13
-    if (event.keyCode === 13 || event.which == 13) {
+
+  var setupEventListeners = function () {
+    var DOM = uiController.getDOMstrings();
+
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+
+    document.addEventListener("keypress", function (event) {
+      //Deer uyin browser deer ajillahin tuld event.which==13
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: function () {
+      console.log("Application started...");
+      setupEventListeners();
+    },
+  };
 })(uiController, financeController);
+
+appController.init();
